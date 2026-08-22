0aa5c3f3638714bb4538b7e245bcb84fba432e7d
Shahbaz Ehfaz <mrshahbaz604@gmail.com>
2026-08-22 09:46:45 +0530

    Create Basic.java

---
+++ Basic.java
@@ -0,0 +1,10 @@
+package Basicofjava;
+
+public class basics {
+    public static void main(String[] args) {
+        System.out.println("hello shahbaz");
+        System.out.println("hello java");
+    }
+
+    
+}
