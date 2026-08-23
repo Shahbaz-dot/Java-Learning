fed68486c5405517e2b99ac9ad456112dbba81ac
Shahbaz Ehfaz <mrshahbaz604@gmail.com>
2026-08-23 10:25:59 +0530

    Create Arithmetic.java

---
+++ Arithmetic.java
@@ -0,0 +1,16 @@
+package Basicofjava;
+
+public class arithamatics {
+    public static void main(String[] args) {
+        //  arithmatics opertor +-*/
+
+        int x = 5;
+        int y = 2;
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+        
+    }
+    
+}
