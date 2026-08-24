274c8079e2b962c309cc5c7d92209e1f725fa630
Shahbaz Ehfaz <mrshahbaz604@gmail.com>
2026-08-24 12:21:58 +0530

    Create arithmeticdouble.java

---
+++ arithmeticdouble.java
@@ -0,0 +1,14 @@
+package Basicofjava;
+
+public class arithmaticdouble {
+    
+    public static void main(String[] args) {
+        
+        double x = 7;
+        double y = 5;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+    }
+} 
