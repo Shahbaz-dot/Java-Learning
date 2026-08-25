227c21d054b047f83811cf72d51a3f0bc3ebf920
Shahbaz Ehfaz <mrshahbaz604@gmail.com>
2026-08-25 13:50:17 +0530

    Create Doubledatatypes.java

---
+++ Doubledatatypes.java
@@ -0,0 +1,21 @@
+package Basicofjava;
+
+public class doubledatatype {
+    
+    public static void main(String[] args) {
+
+        // reak/real = real
+        double x = 5;
+        double y = 2;
+        double z = x / y;
+        System.out.println(z);
+
+
+           
+// int /int = int
+  //      double a = 5;
+    //     double b = 2;
+    //     double c = 5 / 2;
+    //     System.out.println(c);
+    }
+} 
