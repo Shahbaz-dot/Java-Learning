66f126d881b6c1b046eb553f6adc051ec04b6161
Shahbaz Ehfaz <mrshahbaz604@gmail.com>
2026-08-26 14:41:32 +0530

    Create IncrementDecrement.java

---
+++ IncrementDecrement.java
@@ -0,0 +1,34 @@
+package Basicofjava;
+
+public class incrementDecrement {
+    public static void main(String[] args) {
+        // Incriment and decrement
+
+        int x = 5;
+        // increment
+        x ++; // x++ or ++x means x = x + 1;
+        ++ x;
+        System.out.println(x);
+
+        // decrement
+        x--;
+        System.out.println(x);
+
+        int y = 4;
+        System.out.println(y);
+        y++;
+        // --y;
+        System.out.println(y);
+
+        int z = 7;
+        System.out.println(z++);
+        // x++ print then incriment
+        System.out.println(z);
+
+         int a = 5;
+        System.out.println(++a);
+        // ++a increment the use
+        System.out.println(a);
+    }
+    
+}
