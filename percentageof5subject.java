492ca31f527d371e3cbd05ca1b6a9609f283f35c
Shahbaz Ehfaz <mrshahbaz604@gmail.com>
2026-08-28 20:36:49 +0530

    Create percentageof5subject.java

---
+++ percentageof5subject.java
@@ -0,0 +1,18 @@
+package Basicofjava;
+
+public class percentageof5subject {
+    public static void main(String[] args) {
+        // code for calculating percentage
+        // shahbaz alam 12%
+        double Math = 72; // maths
+        double English = 72; //english
+        double Hindi = 78; // hindi
+        double Physics = 60; // physics
+        double Chemistry =  79; // chemistry
+
+        double p = (Math + English + Hindi + Physics + Chemistry)/5;
+        System.out.println(p);
+
+    }
+    
+} 
