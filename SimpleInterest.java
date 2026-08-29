1828d1b238332e9a60783f206f7fc1b81ab9ea88
Shahbaz Ehfaz <mrshahbaz604@gmail.com>
2026-08-30 01:37:42 +0530

    Create SimpleInterest.java

---
+++ SimpleInterest.java
@@ -0,0 +1,14 @@
+package Basicofjava;
+
+public class simpleintrest {
+
+    public static void main(String[] args) {
+        
+        double principle = 100;
+        double rate = 20;
+        double time = 10;
+        double Simple_Intrest = (principle*rate*time)/100;
+        System.out.println(Simple_Intrest);
+    }
+    
+}
