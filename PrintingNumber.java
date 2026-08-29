a13b6261e1c5c0820e3e599d779220da2e1b509c
Shahbaz Ehfaz <mrshahbaz604@gmail.com>
2026-08-29 18:51:05 +0530

    Create PrintingNumber.java

---
+++ PrintingNumber.java
@@ -0,0 +1,11 @@
+package Basicofjava;
+
+public class printingnumber {
+    public static void main(String[] args) {
+        System.out.println(4);
+        System.out.println(4+7);
+        System.out.println("4");
+        System.out.println("4+7");
+    }
+    
+} 
