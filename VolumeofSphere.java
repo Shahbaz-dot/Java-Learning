1dfeb2d7c19cadd0629bd82e93ea8d28b3522e31
Shahbaz Ehfaz <mrshahbaz604@gmail.com>
2026-09-02 10:25:06 +0530

    Create VolumeofSphere.java

---
+++ VolumeofSphere.java
@@ -0,0 +1,16 @@
+package Basicofjava;
+
+public class volumeofsphere {
+    
+    public static void main(String[] args) {
+
+        // volume of sphere
+        double r$ = 5;
+        double pi = 3.14;
+
+        double volume = 1.33*pi*r$*r$;
+        System.out.println("The Volume of Sphere is : ");
+        System.out.println(volume);
+        
+    }
+}
