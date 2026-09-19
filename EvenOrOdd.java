526ec5a5d801c73385b439af06756c3c6ea4c230
Shahbaz Ehfaz <mrshahbaz604@gmail.com>
2026-09-19 09:20:18 +0530

    Create EvenOrOdd.java

---
+++ EvenOrOdd.java
@@ -0,0 +1,19 @@
+package Conditionalstatements;
+
+import java.util.Scanner;
+
+public class evenodd {
+   public evenodd() {
+   }
+
+   public static void main(String[] var0) {
+      Scanner var1 = new Scanner(System.in);
+      System.out.println("Enter a number : ");
+      int var2 = var1.nextInt();
+      if (var2 % 2 == 0) {
+         System.out.println("Even");
+         var1.close();
+      }
+
+   }
+}
